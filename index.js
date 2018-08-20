a3300df5f64fb8d69ad51716ef3ed9fb66d107f5
Cara Morelli <caramorelli7@gmail.com>
2018-08-20 01:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function nowServing(line_arr) {
   if (line_arr.length === 0) {
     return 'There is nobody waiting to be served!'
   }
-  var current_customer = line_arr[0];
-  line_arr.shift;
+  var current_customer = line_arr.shift;
   return 'Currently serving ' + current_customer + '.';
   
 }
